810dd3e2ed27f2fb76d0280c7cf46bd14e9d5807
unknown <lemoor@o2.pl>
2021-03-08 17:10:50 +0100

    l1

--- l1/main.c
+++ l1/main.c
@@ -2,9 +2,27 @@
 
 int main(int argc, char** argv)
 {
-    int arr[10]; // deklaracja statycznej tablicy 10-elementowej typu int
+    int size;
 
-    scanf_s("%d");
+    printf("Podaj liczbe: \n> ");
+    if (!scanf_s("%d", &size))
+    {
+        printf("\nKatastrofalny blad przy wprowadzaniu danych!!!\n");
+        return -1;
+    }
 
+    for (int i = 0; i < size; i++)
+    {
+        int stars = i+1;
+        int spaces = size - stars;
+
+        for (int j = 0; j < spaces; j++)
+            printf(" ");
+        
+        for (int j = 0; j < 2 * stars - 1; j++)
+            printf("*");
+        
+        printf("\n");
+    }       
     return 0;
 }
