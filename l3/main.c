2687d1d7cda66e9e2e465ea6862bdc22bfc80c70
unknown <lemoor@o2.pl>
2021-03-15 16:29:31 +0100

    memory leak fixed, $#%*&!

--- l3/main.c
+++ l3/main.c
@@ -3,23 +3,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-bool isPrime(unsigned int num)
-{
-    if (num == 2)
-        return true;
-
-    if (num % 2 == 0)
-        return false;
-
-    for (int i = 3; i * i <= num; i += 2)
-    {
-        if (num % i == 0)
-            return false;
-    }
-
-    return true;
-}
-
 int fibonacciAux(unsigned int num, int* previous)
 {
     if (num == 1 || num == 2)
@@ -42,7 +25,10 @@ int fibonacci(unsigned int num)
     
     previous[0] = 1;
     previous[1] = 1;
-    return fibonacciAux(num, previous);
+
+    int num = fibonacciAux(num, previous);
+    free(previous);
+    return num;
 }
 
 
