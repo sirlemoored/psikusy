e7be0b99c2f5aed725f6925e18f1f42209088679
unknown <lemoor@o2.pl>
2021-03-15 16:24:54 +0100

    fibonacci, but overly ambitious

--- l3/main.c
+++ l3/main.c
@@ -20,11 +20,36 @@ bool isPrime(unsigned int num)
     return true;
 }
 
+int fibonacciAux(unsigned int num, int* previous)
+{
+    if (num == 1 || num == 2)
+        return 1;
+
+    if (previous[num - 1] != 0)
+        return previous[num - 1];
+
+    previous[num - 1] = fibonacciAux(num - 1, previous) + fibonacciAux(num - 2, previous);
+    return previous[num - 1];
+
+    return 0;
+}
+
+int fibonacci(unsigned int num)
+{
+    int* previous = calloc(num, sizeof(unsigned int));
+    if (previous == NULL)
+        return -1;
+    
+    previous[0] = 1;
+    previous[1] = 1;
+    return fibonacciAux(num, previous);
+}
+
 
 int main(int argc, char** argv) 
 {
     if (argc != 2)
         printf("Invalid number of arguments. Usage: <number> \n");
-    printf("Primality of %s: %d\n", argv[1], isPrime(atoi(argv[1])));
+    printf("Fibonacci(%s) = %d\n", argv[1], fibonacci(atoi(argv[1])));
     return 0;
 }
